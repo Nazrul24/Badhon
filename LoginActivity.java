68279064f770d11d18e64712805cdbc8c0fa4568
Nazrul24 <44968580+Nazrul24@users.noreply.github.com>
2019-10-15 07:21:24 +0600

    Login Complete

--- LoginActivity.java
+++ LoginActivity.java
@@ -1,19 +1,36 @@
+
 package kazinazrul.csedu24.badhon_knih;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
 
 
-    Button LoginButton,NeedNewAccountLink;
-    EditText UserEmail,UserPassword;
+    private Button LoginButton,NeedNewAccountLink;
+    private EditText UserEmail,UserPassword;
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
+
 
 
     @Override
@@ -25,6 +42,8 @@ public class LoginActivity extends AppCompatActivity {
         NeedNewAccountLink = (Button) findViewById(R.id.register_account_link);
         UserEmail = (EditText) findViewById(R.id.login_email);
         UserPassword = (EditText) findViewById(R.id.login_password);
+        mAuth = FirebaseAuth.getInstance();
+        loadingBar = new ProgressDialog(this);
 
         NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,9 +52,75 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AllowingUserToLogin();
+            }
+        });
 
 
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null){
+            SendUserToMainActivity();
+
+        }
+    }
+
+
+    private void AllowingUserToLogin() {
+
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please give your email",Toast.LENGTH_SHORT).show();
+        }
+
+        else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please give your password",Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+
+            loadingBar.setTitle("Logging in");
+            loadingBar.setMessage("Please wait...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+            mAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                SendUserToMainActivity();
+                                Toast.makeText(LoginActivity.this,"Logged in successfully",Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else{
+                                String message = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this,"Error..."+message,Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(LoginActivity.this,MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 
     private void SendUserToRegisterActivity() {
