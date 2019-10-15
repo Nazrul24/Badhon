639fbfe72265be1e6277a6f86aff4c827dbfee3b
Nazrul24 <44968580+Nazrul24@users.noreply.github.com>
2019-10-15 07:22:01 +0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -10,12 +10,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +35,14 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private FirebaseAuth mAuth;
+    private DatabaseReference userRef;
 
+    private CircleImageView NavProfileImage;
+    private TextView NavProfileUserName;
 
+    private ImageButton addNewPostButton;
+
+    String currentUserID;
 
 
 
@@ -42,21 +58,55 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Home");
 
+
         mAuth=FirebaseAuth.getInstance();
 
+        currentUserID = mAuth.getCurrentUser().getUid();
 
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        addNewPostButton =(ImageButton) findViewById(R.id.add_new_post_button);
 
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
         View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+        NavProfileUserName = (TextView) navView.findViewById(R.id.nav_user_full_name);
+        NavProfileImage = (CircleImageView) navView.findViewById(R.id.nav_profile_image);
+
+
         actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        userRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
 
+                    if(dataSnapshot.hasChild("fullname")){
+                        String fullname = dataSnapshot.child("fullname").getValue().toString();
+                        NavProfileUserName.setText(fullname);
+                    }
+                    if(dataSnapshot.hasChild("profileimage")){
+                        String image = dataSnapshot.child("profileimage").getValue().toString();
+                        Picasso.get().load(image).placeholder(R.drawable.profile).into(NavProfileImage);
+                    }
 
+                    else{
+                        Toast.makeText(MainActivity.this,"Profile image does not exist....",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -67,6 +117,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        addNewPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendUserToPostActivity();
+            }
+        });
+
+    }
+
+    private void SendUserToPostActivity() {
+        Intent postIntent = new Intent(MainActivity.this,PostActivity.class);
+        //postIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(postIntent);
+        //finish();
     }
 
     @Override
@@ -78,6 +142,36 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        else {
+            CheckUserExistance();
+        }
+
+    }
+
+    private void CheckUserExistance() {
+        final String current_user_id = mAuth.getCurrentUser().getUid();
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.hasChild(current_user_id)){
+                    SendUserToSetupActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void SendUserToSetupActivity() {
+        Intent setupIntent = new Intent(MainActivity.this,SetupActivity.class);
+        setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(setupIntent);
+        finish();
     }
 
     private void SendUserToLoginActivity() {
@@ -125,11 +219,13 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.nav_post:
-                Toast.makeText(this,"Post", Toast.LENGTH_SHORT).show();
+                SendUserToPostActivity();
                 break;
 
+
             case R.id.nav_logout:
-                Toast.makeText(this,"Logout",Toast.LENGTH_SHORT).show();
+                mAuth.signOut();
+                SendUserToLoginActivity();
                 break;
         }
 
